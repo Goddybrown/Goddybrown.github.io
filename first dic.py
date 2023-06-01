0a6be4b3b3bb0431591df1bbdf16b38e3635cb47
Goddybrown <114139048+Goddybrown@users.noreply.github.com>
2023-06-01 13:58:46 +0100

    Add files via upload
    Python first program

---
+++ first dic.py
@@ -0,0 +1,13 @@
+my_dict = {"firstName" : "Godwin",
+           "middleName" : "Godwin",
+           "Lastname" : "Ita",
+           "date": "May, 2023",
+           "school" : "university 0f uyo",
+           "Department" : "Agricultural Engineering",
+            "level" : "400 level",
+           "learning" : "Python"}
+print(my_dict.values())
+print()
+
+new_dict = dict(age = "25", Nationality = "Nigerian", city = "uyo")
+print(new_dict.values())
